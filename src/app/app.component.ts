495970480fea6c621cb351d3dd4529b4439d2fc3
itsvishalbobade <116144784+itsvishalbobade@users.noreply.github.com>
2023-02-18 15:41:44 +0530

    p tag insert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{ FormControl,FormGroup,FormControlName,Validator, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fromvalidate';
+  signin= new FormGroup ({
+    name:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)]),
+    email:new FormControl('',[Validators.required,]),
+    number:new FormControl('',[Validators.required,Validators.minLength(10)]),
+    file:new FormControl ('', [Validators.required,]),
+  })
+  user(){
+    console.log(this.signin.value)
+  }
+  get name(){
+    return this.signin.get('name')
+
+  }
+  get password(){
+    return this.signin.get('password')
+  }
+  get email(){
+    return this.signin.get('email')
+  }
+  get number(){
+    return this.signin.get('number')
+  }
+  get file(){
+    return this.signin.get('file')
+  }
 }
